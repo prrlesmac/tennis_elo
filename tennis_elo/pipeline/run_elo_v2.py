a4bcc2c273469fdbcac24605e98857f20f0fba77
prrlesmac <prrlesmac@gmail.com>
2024-04-05 00:34:43 -0600

    fix file extensions

--- tennis_elo/pipeline/run_elo_v2.py
+++ tennis_elo/pipeline/run_elo_v2.py
@@ -23,8 +23,8 @@ players_master = open_github_csv_as_dataframe(
 matches, elos = calculate_elo_history(wta_matches, 20)
 current_elos = get_current_elo(players_master, elos)
 
-if not os.path.exists("data/04_model_output"):
-    os.makedirs("data/04_model_output")
-current_elos.to_csv("data/04_model_output/current_elos_wta.csv")
+if not os.path.exists("/data/04_model_output"):
+    os.makedirs("/data/04_model_output")
+current_elos.to_csv("/data/04_model_output/current_elos_wta.csv")
 
 print(current_elos)
